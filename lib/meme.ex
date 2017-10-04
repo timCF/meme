387d1454a3dfe0a8a51f0c2ede4da8aa999fb880
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2017-10-04 15:40:56 +0300

    implement defmemo / defpmemo macro

--- lib/meme.ex
+++ lib/meme.ex
@@ -1,5 +1,84 @@
 defmodule Meme do
 
+  #
+  # public macro to use instead of def
+  #
+
+  defmacro defmemo(f_head = {:when, _, [{f_name, _, f_args} | _]}, [timeout: timeout], [do: body]) do
+    quote do
+      def unquote(f_head) do
+        {
+          __MODULE__,
+          unquote(f_name),
+          unquote(f_args)
+        }
+        |> meme_under_the_hood(unquote(timeout), unquote(body))
+      end
+    end
+  end
+  defmacro defmemo(f_head = {f_name, _, f_args}, [timeout: timeout], [do: body]) do
+    quote do
+      def unquote(f_head) do
+        {
+          __MODULE__,
+          unquote(f_name),
+          unquote(f_args)
+        }
+        |> meme_under_the_hood(unquote(timeout), unquote(body))
+      end
+    end
+  end
+
+  #
+  # public macro to use instead of defp
+  #
+
+  defmacro defpmemo(f_head = {:when, _, [{f_name, _, f_args} | _]}, [timeout: timeout], [do: body]) do
+    quote do
+      defp unquote(f_head) do
+        {
+          __MODULE__,
+          unquote(f_name),
+          unquote(f_args)
+        }
+        |> meme_under_the_hood(unquote(timeout), unquote(body))
+      end
+    end
+  end
+  defmacro defpmemo(f_head = {f_name, _, f_args}, [timeout: timeout], [do: body]) do
+    quote do
+      defp unquote(f_head) do
+        {
+          __MODULE__,
+          unquote(f_name),
+          unquote(f_args)
+        }
+        |> meme_under_the_hood(unquote(timeout), unquote(body))
+      end
+    end
+  end
+
+  #
+  # it's public only for defmemo / defpmemo macro
+  #
+
+  defmacro meme_under_the_hood(key, timeout, code) do
+    quote do
+      case Cachex.get(:meme, unquote(key)) do
+        {:missing, nil} ->
+          value = (unquote(code))
+          {:ok, true} = Cachex.set(:meme, unquote(key), value, [ttl: unquote(timeout)])
+          value
+        {:ok, value} ->
+          value
+      end
+    end
+  end
+
+  #
+  # simple public function for MFA usage
+  #
+
   def memo(module, func, args, ttl) when (is_atom(module) and is_atom(func) and is_list(args) and is_integer(ttl) and (ttl > 0)) do
     key = {module, func, args}
     case Cachex.get(:meme, key) do
--- test/meme_test.exs
+++ test/meme_test.exs
@@ -1,13 +1,62 @@
 defmodule MemeTest do
   use ExUnit.Case
+  import Meme
   doctest Meme
 
+  @ttl 1000
+  @randlimit 6000000000
+
   test "memo" do
-    ttl = 1000
-    arg1 = 6000000000
-    result = Meme.memo(:rand, :uniform, [arg1], ttl)
-    assert result == Meme.memo(:rand, :uniform, [arg1], ttl)
-    _ = :timer.sleep(ttl * 2)
-    assert result != Meme.memo(:rand, :uniform, [arg1], ttl)
+    result = Meme.memo(:rand, :uniform, [@randlimit], @ttl)
+    assert result == Meme.memo(:rand, :uniform, [@randlimit], @ttl)
+    _ = :timer.sleep(@ttl * 2)
+    assert result != Meme.memo(:rand, :uniform, [@randlimit], @ttl)
   end
+
+  defmemo rand_public(limit), timeout: @ttl do
+    :rand.uniform(limit)
+  end
+  test "defmemo" do
+    result = __MODULE__.rand_public(@randlimit)
+    assert result == __MODULE__.rand_public(@randlimit)
+    _ = :timer.sleep(@ttl * 2)
+    assert result != __MODULE__.rand_public(@randlimit)
+  end
+
+  defmemo rand_public_when(limit) when (is_integer(limit) and (limit > 0)), timeout: @ttl do
+    :rand.uniform(limit)
+  end
+  test "defmemo + when" do
+    result = __MODULE__.rand_public_when(@randlimit)
+    assert result == __MODULE__.rand_public_when(@randlimit)
+    _ = :timer.sleep(@ttl * 2)
+    assert result != __MODULE__.rand_public_when(@randlimit)
+  end
+
+  defpmemo rand_private(limit), timeout: @ttl do
+    :rand.uniform(limit)
+  end
+  test "defpmemo generates private function" do
+    assert false == :erlang.function_exported(__MODULE__, :rand_private, 1)
+  end
+  test "defpmemo" do
+    result = rand_private(@randlimit)
+    assert result == rand_private(@randlimit)
+    _ = :timer.sleep(@ttl * 2)
+    assert result != rand_private(@randlimit)
+  end
+
+  defpmemo rand_private_when(limit) when (is_integer(limit) and (limit > 0)), timeout: @ttl do
+    :rand.uniform(limit)
+  end
+  test "defpmemo + when generates private function" do
+    assert false == :erlang.function_exported(__MODULE__, :rand_private_when, 1)
+  end
+  test "defpmemo + when" do
+    result = rand_private_when(@randlimit)
+    assert result == rand_private_when(@randlimit)
+    _ = :timer.sleep(@ttl * 2)
+    assert result != rand_private_when(@randlimit)
+  end
+
 end
