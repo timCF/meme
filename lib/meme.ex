4ae80fecad6408c7263bb49ca074dbea7024d420
Ilja Tkachuk <ilja@heathmont.net>
2018-05-10 13:05:04 +0300

    add :condition option, boolean function arity 1 provides conditional caching

--- lib/meme.ex
+++ lib/meme.ex
@@ -4,20 +4,26 @@ defmodule Meme do
   # public macro to use instead of def
   #
 
-  defmacro defmemo({:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout], [do: body]) do
+  defmacro defmemo(def_ast, [timeout: timeout], [do: body]) do
+    quote do
+      defmemo(unquote(def_ast), [timeout: unquote(timeout), condition: fn(_) -> true end], [do: unquote(body)])
+    end
+  end
+
+  defmacro defmemo({:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     quote do
       def unquote({:when, when_meta, [{name, meta, decorated_args} | guards]}) do
-        unquote(meme_under_the_hood(name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(name, arg_names, timeout, body, condition))
       end
     end
   end
 
-  defmacro defmemo({name, meta, raw_args}, [timeout: timeout], [do: body]) do
+  defmacro defmemo({name, meta, raw_args}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     quote do
       def unquote({name, meta, decorated_args}) do
-        unquote(meme_under_the_hood(name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(name, arg_names, timeout, body, condition))
       end
     end
   end
@@ -28,12 +34,18 @@ defmodule Meme do
   # :foo and :cached_foo (arity, arguments and business logic is the same)
   #
 
-  defmacro defcached(raw_definition = {:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout], [do: body]) do
+  defmacro defcached(def_ast, [timeout: timeout], [do: body]) do
+    quote do
+      defcached(unquote(def_ast), [timeout: unquote(timeout), condition: fn(_) -> true end], [do: unquote(body)])
+    end
+  end
+
+  defmacro defcached(raw_definition = {:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     cached_name = String.to_atom("cached_#{name}")
     quote do
       def unquote({:when, when_meta, [{cached_name, meta, decorated_args} | guards]}) do
-        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body, condition))
       end
       def unquote(raw_definition) do
         unquote(body)
@@ -41,12 +53,12 @@ defmodule Meme do
     end
   end
 
-  defmacro defcached(raw_definition = {name, meta, raw_args}, [timeout: timeout], [do: body]) do
+  defmacro defcached(raw_definition = {name, meta, raw_args}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     cached_name = String.to_atom("cached_#{name}")
     quote do
       def unquote({cached_name, meta, decorated_args}) do
-        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body, condition))
       end
       def unquote(raw_definition) do
         unquote(body)
@@ -59,20 +71,26 @@ defmodule Meme do
   # public macro to use instead of defp
   #
 
-  defmacro defmemop({:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout], [do: body]) do
+  defmacro defmemop(def_ast, [timeout: timeout], [do: body]) do
+    quote do
+      defmemop(unquote(def_ast), [timeout: unquote(timeout), condition: fn(_) -> true end], [do: unquote(body)])
+    end
+  end
+
+  defmacro defmemop({:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     quote do
       defp unquote({:when, when_meta, [{name, meta, decorated_args} | guards]}) do
-        unquote(meme_under_the_hood(name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(name, arg_names, timeout, body, condition))
       end
     end
   end
 
-  defmacro defmemop({name, meta, raw_args}, [timeout: timeout], [do: body]) do
+  defmacro defmemop({name, meta, raw_args}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     quote do
       defp unquote({name, meta, decorated_args}) do
-        unquote(meme_under_the_hood(name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(name, arg_names, timeout, body, condition))
       end
     end
   end
@@ -81,12 +99,18 @@ defmodule Meme do
   # public macro - creates functions like defcached do, but private
   #
 
-  defmacro defcachedp(raw_definition = {:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout], [do: body]) do
+  defmacro defcachedp(def_ast, [timeout: timeout], [do: body]) do
+    quote do
+      defcachedp(unquote(def_ast), [timeout: unquote(timeout), condition: fn(_) -> true end], [do: unquote(body)])
+    end
+  end
+
+  defmacro defcachedp(raw_definition = {:when, when_meta, [{name, meta, raw_args} | guards]}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     cached_name = String.to_atom("cached_#{name}")
     quote do
       defp unquote({:when, when_meta, [{cached_name, meta, decorated_args} | guards]}) do
-        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body, condition))
       end
       defp unquote(raw_definition) do
         unquote(body)
@@ -94,12 +118,12 @@ defmodule Meme do
     end
   end
 
-  defmacro defcachedp(raw_definition = {name, meta, raw_args}, [timeout: timeout], [do: body]) do
+  defmacro defcachedp(raw_definition = {name, meta, raw_args}, [timeout: timeout, condition: condition], [do: body]) do
     {arg_names, decorated_args} = decorate_args(raw_args)
     cached_name = String.to_atom("cached_#{name}")
     quote do
       defp unquote({cached_name, meta, decorated_args}) do
-        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body))
+        unquote(meme_under_the_hood(cached_name, arg_names, timeout, body, condition))
       end
       defp unquote(raw_definition) do
         unquote(body)
@@ -127,14 +151,18 @@ defmodule Meme do
   # priv boilerplate for defmemo / defmemop macro
   #
 
-  defp meme_under_the_hood(name, args, timeout, code) do
+  defp meme_under_the_hood(name, args, timeout, code, condition) do
     quote do
       (
         key = {__MODULE__, unquote(name), unquote(args)}
         case Cachex.get(:meme, key) do
           {:missing, nil} ->
             value = (unquote(code))
-            {:ok, true} = Cachex.set(:meme, key, value, [ttl: unquote(timeout)])
+            (unquote(condition)).(value)
+            |> case do
+              true -> {:ok, true} = Cachex.set(:meme, key, value, [ttl: unquote(timeout)])
+              false -> :ok
+            end
             value
           {:ok, value} ->
             value
